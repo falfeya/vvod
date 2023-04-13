f09538e62a3693963b35377b49d72a4f1300f5cf
falfeya <130572121+falfeya@users.noreply.github.com>
2023-04-13 13:38:57 +0500

    Add files via upload

---
+++ практическая ввод/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace практическая_ввод
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ф.И.О: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите mail: ");
+            string mail = Console.ReadLine();
+            Console.WriteLine("Введите баллы по математике: ");
+            double program = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите баллы по программированию: ");
+            double math = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите баллы по физике: ");
+            double phys = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Ф.И.О.: {fullName}");
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"mail {mail}");
+            Console.WriteLine($"Баллы по программированию: {program}");
+            Console.WriteLine($"Баллы по математике: {math}");
+            Console.WriteLine($"Баллы по физике: {phys}");
+            Console.ReadLine();
+        }
+    }
+}
